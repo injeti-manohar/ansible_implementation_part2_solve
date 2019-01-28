06477d84ea686b887315cdd7ac31720c2ede6cee
root <root@bastion.3700.example.opentlc.com>
2019-01-28 17:10:43 +0000

    update

---
+++ files/version_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from ansible.module_utils.basic import *
+
+def main():
+
+    fields = {
+        "version_no": {"default": True, "type": "str"},
+        "version_name": {"default": True, "type": "str"},
+        "unchanged_value": {"default": True, "type": "str"}
+    }
+
+    module = AnsibleModule(argument_spec=fields)
+    module.params.update({"version_name": "After"})
+    mylist = module.params["version_no"].split('.')
+    mylist[2] = str(int(mylist[2]) + 2)
+    mylist[1] = str(int(mylist[1]) + 1)
+    mystr= '.'.join(mylist)
+    module.params.update({"version_no": mystr})
+
+
+    module.exit_json(changed=True, meta=module.params)
+
+
+if __name__ == '__main__':
+    main()
+
